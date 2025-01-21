cdf3692d63c53df3a7d0f33e2ad03cfd66d44738
UsamiLyan <1364563060@qq.com>
2025-01-21 14:48:30 +0800

    Add files via upload

--- 00-Other/Func.cs
+++ 00-Other/Func.cs
@@ -15,6 +15,7 @@ using ECommons.GameFunctions;
 using KodakkuAssist.Script;
 using KodakkuAssist.Module.GameEvent;
 using KodakkuAssist.Module.Draw;
+using KodakkuAssist.Module.Draw.Manager;
 using FFXIVClientStructs.FFXIV.Client.Game.Character;
 
 namespace UsamisScript;
@@ -92,7 +93,7 @@ public static class EventExtensions
     {
         return ParseHexId(@event["TargetId"], out var id) ? id : 0;
     }
-    
+
     public static uint TargetIndex(this Event @event)
     {
         return JsonConvert.DeserializeObject<uint>(@event["TargetIndex"]);
@@ -207,6 +208,12 @@ public static class DirectionCalc
     // 1.5 pi       - 6     1.5pi
     // 1.75 pi      - 5     1.25pi
     // Dir = Pi - InnGame (+ 2pi)
+
+    /// <summary>
+    /// 将游戏基角度（以南为0，逆时针增加）转为逻辑基角度（以北为0，顺时针增加）
+    /// </summary>
+    /// <param name="radian">游戏基角度</param>
+    /// <returns>逻辑基角度</returns>
     public static float BaseInnGame2DirRad(float radian)
     {
         float r = (float)Math.PI - radian;
@@ -215,6 +222,11 @@ public static class DirectionCalc
         return r;
     }
 
+    /// <summary>
+    /// 将逻辑基角度（以北为0，顺时针增加）转为游戏基角度（以南为0，逆时针增加）
+    /// </summary>
+    /// <param name="radian">逻辑基角度</param>
+    /// <returns>游戏基角度</returns>
     public static float BaseDirRad2InnGame(float radian)
     {
         float r = (float)Math.PI - radian;
@@ -223,6 +235,12 @@ public static class DirectionCalc
         return r;
     }
 
+    /// <summary>
+    /// 输入逻辑基角度，获取逻辑方位
+    /// </summary>
+    /// <param name="radian">逻辑基角度</param>
+    /// <param name="dirs">方位总数</param>
+    /// <returns>逻辑基角度对应的逻辑方位</returns>
     public static int DirRadRoundToDirs(float radian, int dirs)
     {
         var r = Math.Round(radian / (2f / dirs * Math.PI));
@@ -230,6 +248,13 @@ public static class DirectionCalc
         return (int)r;
     }
 
+    /// <summary>
+    /// 输入坐标，获取正分割逻辑方位（以右上为0）
+    /// </summary>
+    /// <param name="point">坐标点</param>
+    /// <param name="center">中心点</param>
+    /// <param name="dirs">方位总数</param>
+    /// <returns>该坐标点对应的逻辑方位</returns>
     public static int PositionFloorToDirs(Vector3 point, Vector3 center, int dirs)
     {
         // 正分割，0°为分界线，将360°分为dirs份
@@ -237,6 +262,13 @@ public static class DirectionCalc
         return (int)r;
     }
 
+    /// <summary>
+    /// 输入坐标，获取斜分割逻辑方位（以正上为0）
+    /// </summary>
+    /// <param name="point">坐标点</param>
+    /// <param name="center">中心点</param>
+    /// <param name="dirs">方位总数</param>
+    /// <returns>该坐标点对应的逻辑方位</returns>
     public static int PositionRoundToDirs(Vector3 point, Vector3 center, int dirs)
     {
         // 斜分割，0° return 0，将360°分为dirs份
@@ -244,6 +276,11 @@ public static class DirectionCalc
         return (int)r;
     }
 
+    /// <summary>
+    /// 将角度转为弧度
+    /// </summary>
+    /// <param name="angle">角度值</param>
+    /// <returns>对应的弧度值</returns>
     public static float angle2Rad(float angle)
     {
         // 输入角度转为弧度
@@ -251,6 +288,13 @@ public static class DirectionCalc
         return radian;
     }
 
+    /// <summary>
+    /// 以逻辑基弧度旋转某点
+    /// </summary>
+    /// <param name="point">待旋转点坐标</param>
+    /// <param name="center">中心</param>
+    /// <param name="radian">旋转弧度</param>
+    /// <returns>旋转后坐标点</returns>
     public static Vector3 RotatePoint(Vector3 point, Vector3 center, float radian)
     {
         // 围绕某点顺时针旋转某弧度
@@ -263,12 +307,25 @@ public static class DirectionCalc
         // var nextPos = Vector3.Transform((point - center), Matrix4x4.CreateRotationY(radian)) + center;
     }
 
+    /// <summary>
+    /// 以逻辑基角度从某中心点向外延伸
+    /// </summary>
+    /// <param name="center">待延伸中心点</param>
+    /// <param name="radian">旋转弧度</param>
+    /// <param name="length">延伸长度</param>
+    /// <returns>延伸后坐标点</returns>
     public static Vector3 ExtendPoint(Vector3 center, float radian, float length)
     {
         // 令某点以某弧度延伸一定长度
         return new(center.X + MathF.Sin(radian) * length, center.Y, center.Z - MathF.Cos(radian) * length);
     }
 
+    /// <summary>
+    /// 寻找外侧某点到中心的逻辑基弧度
+    /// </summary>
+    /// <param name="center">中心</param>
+    /// <param name="new_point">外侧点</param>
+    /// <returns>外侧点到中心的逻辑基弧度</returns>
     public static float FindRadian(Vector3 center, Vector3 new_point)
     {
         // 找到某点到中心的弧度
@@ -277,38 +334,50 @@ public static class DirectionCalc
             radian += 2 * MathF.PI;
         return radian;
     }
+
+    /// <summary>
+    /// 将输入点左右折叠
+    /// </summary>
+    /// <param name="point">待折叠点</param>
+    /// <param name="centerx">中心折线坐标点</param>
+    /// <returns></returns>
+    public static Vector3 FoldPointLR(Vector3 point, int centerx)
+    {
+        Vector3 v3 = new(2 * centerx - point.X, point.Y, point.Z);
+        return v3;
+    }
 }
 
 public static class IndexHelper
 {
+    /// <summary>
+    /// 输入玩家dataid，获得对应的位置index
+    /// </summary>
+    /// <param name="pid">玩家SourceId</param>
+    /// <param name="accessory"></param>
+    /// <returns>该玩家对应的位置index</returns>
     public static int getPlayerIdIndex(uint pid, ScriptAccessory accessory)
     {
         // 获得玩家 IDX
         return accessory.Data.PartyList.IndexOf(pid);
     }
 
+    /// <summary>
+    /// 获得主视角玩家对应的位置index
+    /// </summary>
+    /// <param name="accessory"></param>
+    /// <returns>主视角玩家对应的位置index</returns>
     public static int getMyIndex(ScriptAccessory accessory)
     {
         return accessory.Data.PartyList.IndexOf(accessory.Data.Me);
     }
 
-    public static string getPlayerJobIndex(uint pid, ScriptAccessory accessory)
-    {
-        // 获得玩家职能简称，无用处，仅作DEBUG输出
-        var a = accessory.Data.PartyList.IndexOf(pid);
-        switch (a)
-        {
-            case 0: return "MT";
-            case 1: return "ST";
-            case 2: return "H1";
-            case 3: return "H2";
-            case 4: return "D1";
-            case 5: return "D2";
-            case 6: return "D3";
-            case 7: return "D4";
-            default: return "unknown";
-        }
-    }
+    /// <summary>
+    /// 输入玩家dataid，获得对应的位置称呼，输出字符仅作文字输出用
+    /// </summary>
+    /// <param name="pid">玩家SourceId</param>
+    /// <param name="accessory"></param>
+    /// <returns>该玩家对应的位置称呼</returns>
     public static string getPlayerJobByID(uint pid, ScriptAccessory accessory)
     {
         // 获得玩家职能简称，无用处，仅作DEBUG输出
@@ -326,6 +395,12 @@ public static class IndexHelper
             default: return "unknown";
         }
     }
+
+    /// <summary>
+    /// 输入位置index，获得对应的位置称呼，输出字符仅作文字输出用
+    /// </summary>
+    /// <param name="idx">位置index</param>
+    /// <returns></returns>
     public static string getPlayerJobByIndex(int idx)
     {
         switch (idx)
@@ -348,6 +423,198 @@ public static class ColorHelper
     public static ScriptColor colorRed = new ScriptColor { V4 = new Vector4(1.0f, 0f, 0f, 1.0f) };
     public static ScriptColor colorPink = new ScriptColor { V4 = new Vector4(1f, 0f, 1f, 1.0f) };
     public static ScriptColor colorCyan = new ScriptColor { V4 = new Vector4(0f, 1f, 0.8f, 1.0f) };
+
+    // 门神 龙龙凤凤延迟危险区颜色，紫色
+    public static ScriptColor DelayDangerColor = new ScriptColor { V4 = new Vector4(1f, 0.2f, 1f, 1.5f) };
+    // 门神 蛇蛇位置颜色
+    public static ScriptColor GorgonColor = new ScriptColor { V4 = new Vector4(1f, 1f, 1f, 2f) };
+}
+
+public static class AssignDp
+{
+    /// <summary>
+    /// 返回自己指向某目标地点的dp，可修改dp.TargetPosition, dp.Scale
+    /// </summary>
+    /// <param name="target_pos">指向地点</param>
+    /// <param name="delay">延时delay ms出现</param>
+    /// <param name="destoryAt">绘图自出现起，经destoryAt ms消失</param>
+    /// <param name="name">绘图名称</param>
+    /// <param name="accessory"></param>
+    /// <returns></returns>
+    public static DrawPropertiesEdit dirPos(Vector3 target_pos, int delay, int destoryAt, string name, ScriptAccessory accessory)
+    {
+        var dp = accessory.Data.GetDefaultDrawProperties();
+        dp.Name = name;
+        dp.Scale = new(0.5f);
+        dp.Owner = accessory.Data.Me;
+        dp.TargetPosition = target_pos;
+        dp.ScaleMode = ScaleMode.YByDistance;
+        dp.Color = accessory.Data.DefaultSafeColor;
+        dp.Delay = delay;
+        dp.DestoryAt = destoryAt;
+        return dp;
+    }
+
+    /// <summary>
+    /// 返回起始地点指向某目标地点的dp，可修改dp.Position, dp.TargetPosition, dp.Scale
+    /// </summary>
+    /// <param name="start_pos">起始地点</param>
+    /// <param name="target_pos">指向地点</param>
+    /// <param name="delay">延时delay ms出现</param>
+    /// <param name="destoryAt">绘图自出现起，经destoryAt ms消失</param>
+    /// <param name="name">绘图名称</param>
+    /// <param name="accessory"></param>
+    /// <returns></returns>
+    public static DrawPropertiesEdit dirPos2Pos(Vector3 start_pos, Vector3 target_pos, int delay, int destoryAt, string name, ScriptAccessory accessory)
+    {
+        var dp = accessory.Data.GetDefaultDrawProperties();
+        dp.Name = name;
+        dp.Scale = new(0.5f);
+        dp.Position = start_pos;
+        dp.TargetPosition = target_pos;
+        dp.ScaleMode = ScaleMode.YByDistance;
+        dp.Color = accessory.Data.DefaultSafeColor;
+        dp.Delay = delay;
+        dp.DestoryAt = destoryAt;
+        return dp;
+    }
+
+    /// <summary>
+    /// 返回自己指向某目标对象的dp，可修改dp.TargetObject, dp.Scale
+    /// </summary>
+    /// <param name="target_id">指向目标对象</param>
+    /// <param name="delay">延时delay ms出现</param>
+    /// <param name="destoryAt">绘图自出现起，经destoryAt ms消失</param>
+    /// <param name="name">绘图名称</param>
+    /// <param name="accessory"></param>
+    /// <returns></returns>
+    public static DrawPropertiesEdit dirTarget(uint target_id, int delay, int destoryAt, string name, ScriptAccessory accessory)
+    {
+        var dp = accessory.Data.GetDefaultDrawProperties();
+        dp.Name = name;
+        dp.Scale = new(0.5f);
+        dp.Owner = accessory.Data.Me;
+        dp.TargetObject = target_id;
+        dp.ScaleMode = ScaleMode.YByDistance;
+        dp.Color = accessory.Data.DefaultSafeColor;
+        dp.Delay = delay;
+        dp.DestoryAt = destoryAt;
+        return dp;
+    }
+
+    /// <summary>
+    /// 返回与某对象仇恨相关的dp，可修改dp.TargetResolvePattern, dp.TargetOrderIndex, dp.Owner
+    /// </summary>
+    /// <param name="owner_id">起始目标id，通常为boss</param>
+    /// <param name="order_idx">顺序，从1开始</param>
+    /// <param name="delay">延时delay ms出现</param>
+    /// <param name="destoryAt">绘图自出现起，经destoryAt ms消失</param>
+    /// <param name="name">绘图名称</param>
+    /// <param name="accessory"></param>
+    /// <returns></returns>
+    public static DrawPropertiesEdit drawTargetOrder(uint owner_id, uint order_idx, int delay, int destoryAt, string name, ScriptAccessory accessory)
+    {
+        var dp = accessory.Data.GetDefaultDrawProperties();
+        dp.Name = name;
+        dp.Scale = new(5, 40);
+        dp.Owner = owner_id;
+        dp.TargetResolvePattern = PositionResolvePatternEnum.PlayerNearestOrder;
+        dp.TargetOrderIndex = order_idx;
+        dp.Color = accessory.Data.DefaultDangerColor;
+        dp.Delay = delay;
+        dp.DestoryAt = destoryAt;
+        return dp;
+    }
+
+    /// <summary>
+    /// 返回与某对象距离相关的dp，可修改dp.CentreResolvePattern, dp.CentreOrderIndex, dp.Owner
+    /// </summary>
+    /// <param name="owner_id">起始目标id，通常为boss</param>
+    /// <param name="order_idx">顺序，从1开始</param>
+    /// <param name="delay">延时delay ms出现</param>
+    /// <param name="destoryAt">绘图自出现起，经destoryAt ms消失</param>
+    /// <param name="name">绘图名称</param>
+    /// <param name="accessory"></param>
+    /// <returns></returns>
+    public static DrawPropertiesEdit drawCenterOrder(uint owner_id, uint order_idx, int delay, int destoryAt, string name, ScriptAccessory accessory)
+    {
+        var dp = accessory.Data.GetDefaultDrawProperties();
+        dp.Name = name;
+        dp.Scale = new(5);
+        dp.Owner = owner_id;
+        dp.CentreResolvePattern = PositionResolvePatternEnum.PlayerNearestOrder;
+        dp.CentreOrderIndex = order_idx;
+        dp.Color = accessory.Data.DefaultDangerColor;
+        dp.Delay = delay;
+        dp.DestoryAt = destoryAt;
+        return dp;
+    }
+    /// <summary>
+    /// 返回owner与target的dp，可修改 dp.Owner, dp.TargetObject, dp.Scale
+    /// </summary>
+    /// <param name="owner_id">起始目标id，通常为自己</param>
+    /// <param name="target_id">目标单位id</param>
+    /// <param name="delay">延时delay ms出现</param>
+    /// <param name="destoryAt">绘图自出现起，经destoryAt ms消失</param>
+    /// <param name="name">绘图名称</param>
+    /// <param name="accessory"></param>
+    /// <returns></returns>
+    public static DrawPropertiesEdit drawOwner2Target(uint owner_id, uint target_id, int delay, int destoryAt, string name, ScriptAccessory accessory)
+    {
+        var dp = accessory.Data.GetDefaultDrawProperties();
+        dp.Name = name;
+        dp.Scale = new(5, 40);
+        dp.Owner = owner_id;
+        dp.TargetObject = target_id;
+        dp.Color = accessory.Data.DefaultDangerColor;
+        dp.Delay = delay;
+        dp.DestoryAt = destoryAt;
+        return dp;
+    }
+
+    /// <summary>
+    /// 返回圆形dp，跟随owner，可修改 dp.Owner, dp.Scale
+    /// </summary>
+    /// <param name="owner_id">起始目标id，通常为自己或Boss</param>
+    /// <param name="delay">延时delay ms出现</param>
+    /// <param name="destoryAt">绘图自出现起，经destoryAt ms消失</param>
+    /// <param name="name">绘图名称</param>
+    /// <param name="accessory"></param>
+    /// <returns></returns>
+    public static DrawPropertiesEdit drawCircle(uint owner_id, int delay, int destoryAt, string name, ScriptAccessory accessory)
+    {
+        var dp = accessory.Data.GetDefaultDrawProperties();
+        dp.Name = name;
+        dp.Scale = new(5);
+        dp.Owner = owner_id;
+        dp.Color = accessory.Data.DefaultDangerColor;
+        dp.Delay = delay;
+        dp.DestoryAt = destoryAt;
+        return dp;
+    }
+
+    /// <summary>
+    /// 返回静态dp，通常用于指引固定位置。可修改 dp.Position, dp.Rotation, dp.Scale
+    /// </summary>
+    /// <param name="center">起始位置，通常为场地中心</param>
+    /// <param name="radian">旋转角度，以北为0度顺时针</param>
+    /// <param name="delay">延时delay ms出现</param>
+    /// <param name="destoryAt">绘图自出现起，经destoryAt ms消失</param>
+    /// <param name="name">绘图名称</param>
+    /// <param name="accessory"></param>
+    /// <returns></returns>
+    public static DrawPropertiesEdit drawStatic(Vector3 center, float radian, int delay, int destoryAt, string name, ScriptAccessory accessory)
+    {
+        var dp = accessory.Data.GetDefaultDrawProperties();
+        dp.Name = name;
+        dp.Scale = new(5, 20);
+        dp.Position = center;
+        dp.Rotation = DirectionCalc.BaseDirRad2InnGame(radian);
+        dp.Color = accessory.Data.DefaultDangerColor;
+        dp.Delay = delay;
+        dp.DestoryAt = destoryAt;
+        return dp;
+    }
 }
 
 #endregion
