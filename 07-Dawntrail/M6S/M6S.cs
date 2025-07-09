de03cffa4ebe4c3c9b1c4fb7b1bf780de3df5341
连营 <1364563060@qq.com>
2025-07-09 14:19:44 +0800

    TMMD

--- 07-Dawntrail/M6S/M6S.cs
+++ 07-Dawntrail/M6S/M6S.cs
@@ -36,17 +36,17 @@ public class M6S
 {
     const string NoteStr =
     """
-    v0.0.0.11
+    v0.0.0.12
     请先于用户设置中调整整体策略（Game8/CnServer）。
     待完善。
     """;
 
     private const string Name = "M6S [零式阿卡狄亚 中量级2]";
-    private const string Version = "0.0.0.11";
+    private const string Version = "0.0.0.12";
 
     private const string UpdateInfo =
         """
-        1. 再再再次修复火山阶段第二轮塔概率性不指路问题。
+        1. 再再再再次修复火山阶段第二轮塔概率性不指路问题。
         """;
 
     private const bool Debugging = false;
